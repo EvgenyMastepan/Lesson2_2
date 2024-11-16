743c00dcfa759e2056c14f8612e92a99f3467833
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-11-16 14:31:21 +0700

    Is began

---
+++ Lesson2_2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lesson2_2
+//
+//  Created by Evgeny Mastepan on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let viewHeight = view.frame.height
+        let leftIndent = 30
+        let rightIndent = view.frame.width - 30
+        let heightPicture = round(view.frame.height / 6)
+    }
+
+
+}
+
