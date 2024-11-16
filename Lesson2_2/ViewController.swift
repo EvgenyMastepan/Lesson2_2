c0f95cf893900d6cb565ffc083ceccbc508ed133
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-11-16 16:53:07 +0700

    Completed

--- Lesson2_2/ViewController.swift
+++ Lesson2_2/ViewController.swift
@@ -11,12 +11,133 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let viewHeight = view.frame.height
-        let leftIndent = 30
-        let rightIndent = view.frame.width - 30
-        let heightPicture = round(view.frame.height / 6)
+        let leftIndent: CGFloat = 30
+        let widthOf = view.frame.width - leftIndent - leftIndent
+        
+        //===== Incoming Data =====
+        let name: String = "Indian"
+        let surname: String = "Larry"
+        //=========================
+        
+        // Картинки
+        let topImage = ourImage(name: "darkness",
+            place: CGRect(x: 0,
+                          y: 0,
+                          width: view.frame.width,
+                          height: round(view.frame.height / 6)))
+        view.addSubview(topImage)
+        let personImage = ourImage(name: "indian",
+            place: CGRect(x: leftIndent,
+                          y: topImage.frame.maxY + leftIndent,
+                          width: 100,
+                          height: 100),  round: true)
+        view.addSubview(personImage)
+        
+        // Текст
+        let nameLabel = ourLabel(text: name + " " + surname,
+            place: CGRect(x: personImage.frame.maxX + 20,
+                          y: personImage.frame.minY + 20,
+                          width: view.frame.maxX - personImage.frame.maxX - 20 - leftIndent,
+                          height: 19), bold: true)
+        view.addSubview(nameLabel)
+        let addSubscriptionLabel = ourLabel(text: "Добавить описание",
+            place: CGRect(x: leftIndent,
+                          y: personImage.frame.maxY + 50,
+                          width: widthOf,
+                          height: 19))
+        view.addSubview(addSubscriptionLabel)
+        let descriptionTextView = ourTextView(
+            place: CGRect(x: leftIndent,
+                          y: addSubscriptionLabel.frame.maxY + 10,
+                          width: widthOf,
+                          height: 150))
+        view.addSubview(descriptionTextView)
+        let changePasswordLabel = ourLabel(text: "Изменить пароль",
+            place: CGRect(x: leftIndent,
+                          y: descriptionTextView.frame.maxY + 30,
+                          width: widthOf,
+                          height: 19))
+        view.addSubview(changePasswordLabel)
+        let oldPasswordField = ourTextField(text: "Старый пароль",
+            place: CGRect(x: leftIndent,
+                          y: changePasswordLabel.frame.maxY + 20,
+                          width: widthOf,
+                          height: 52))
+        view.addSubview(oldPasswordField)
+        let newPasswordField = ourTextField(text: "Новый пароль",
+            place: CGRect(x: leftIndent,
+                          y: oldPasswordField.frame.maxY + 20,
+                          width: widthOf,
+                          height: 52))
+        view.addSubview(newPasswordField)
+
+        
+        // Кнопки
+        //Выровнял кнопку "редактировать" по правому краю. Чтобы сделать, как на превью, достаточно в поле width значение заменить на 147.
+        let editButton = ourButton(text: "редактировать",
+            place: CGRect(x: personImage.frame.maxX + 20, y: nameLabel.frame.maxY + 10, width: view.frame.maxX - personImage.frame.maxX - 20 - leftIndent, height: 34))
+        view.addSubview(editButton)
+        let saveButton = ourButton(text: "Сохранить", place: CGRect(x: leftIndent, y: view.frame.maxY - 105, width: widthOf, height: 55))
+        view.addSubview(saveButton)
+        
     }
 
+    private func ourImage(name: String, place: CGRect, round: Bool = false) -> UIImageView {
+        let image = UIImageView()
+        image.image = UIImage(named: name)
+        image.frame = place
+        image.contentMode = .scaleAspectFill
+        if round { image.layer.cornerRadius = image.frame.size.width / 2 }
+        image.clipsToBounds = true
+        return image
+    }
+    
+    private func ourLabel(text: String, place: CGRect, bold: Bool = false) -> UILabel {
+        let label = UILabel()
+        label.text = text
+//      Отладочный параметр
+//      label.backgroundColor = .yellow
+        label.frame = place
+        if bold { label.font = UIFont.systemFont(ofSize: 16, weight: .heavy) }
+           else { label.font = UIFont.systemFont(ofSize: 16) }
+        return label
+    }
+    private func ourButton(text: String, place: CGRect) -> UIButton {
+        let button = UIButton()
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .blue
+        button.frame = place
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        return button
+    }
+    
+    private func ourTextField(text: String, place: CGRect) -> UITextField {
+        let textField = UITextField()
+        textField.placeholder = text
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.backgroundColor = .systemGray6
+        textField.frame = place
+        textField.isSecureTextEntry = true
+        textField.layer.cornerRadius = 10
+        textField.layer.masksToBounds = true
+        textField.leftViewMode = .always
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.frame = place
+        return textField
+    }
+    
+    private func ourTextView(place: CGRect) -> UITextView {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 16)
+        textView.backgroundColor = .systemGray6
+        textView.textColor = .black
+        textView.frame = place
+        textView.layer.cornerRadius = 20
+        textView.layer.masksToBounds = true
+        textView.textContainer.lineFragmentPadding = 10
+        return textView
+    }
 
 }
 
