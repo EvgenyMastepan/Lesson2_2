c54cdea1172c1ae5af28dd9c554dfd1065985744
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-11-16 17:39:16 +0700

    minor bugfixes

--- Lesson2_2/ViewController.swift
+++ Lesson2_2/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var descriptionTextView = UITextView()
+    private var descriptionTextView = UITextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
